fc5cfb0db418f6e0dfd35ba41abedc5e6ab01509
xiao <abuzhi@163.com>
2019-07-14 20:53:01 +0800

    bit

--- common-algo/src/test/java/com/xiao/demo/algo/BitOperTest.java
+++ common-algo/src/test/java/com/xiao/demo/algo/BitOperTest.java
@@ -26,6 +26,67 @@ public class BitOperTest {
 
     }
 
+    @Test
+    public void toLow() {
+        int a = 5;
+        System.out.println(BitOper.toBinary(a,4 * 8-1));
+        System.out.println(Integer.toBinaryString(a));
+
+        int b  = 1 << 31 ;
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println(Integer.toString(b));
+
+        long c = 1 << 63;
+        System.out.println(Long.toBinaryString(c));
+
+        int low = 65535;
+        System.out.println(Integer.toBinaryString(low));
+
+        int high = 66666;
+        System.out.println(Integer.toBinaryString(high));
+
+    }
+
+    /**
+     * 计算机中存储负数用的是补码，即原码取反码再+1
+     * 例如： - 1 的取之过程为 ，先取1的原码，再取反，再 + 1
+     *         原码：00000000 00000000 00000000 00000001
+     *         反码：11111111 11111111 11111111 11111110
+     *         补码：11111111 11111111 11111111 11111111
+     *         所以，-1 在计算机中转化为 二进制为补码值
+     */
+    @Test
+    public void intMaxmin() {
+        int b  = 1 << 31 ;
+        System.out.println(Integer.toString(b));
+        System.out.println(Integer.toBinaryString(b));
+
+        b = (1 << 31) + 1;
+        System.out.println(Integer.toString(b));
+        System.out.println(Integer.toBinaryString(b));
+
+        b = (1 << 31)-1;
+        System.out.println(Integer.toString(b));
+        System.out.println(Integer.toBinaryString(b));
+
+        b = (1 << 31) + ((1 << 31)-1);
+        System.out.println(Integer.toString(b));
+        System.out.println(Integer.toBinaryString(b));
+
+        b = -2;
+        System.out.println(Integer.toBinaryString(b));
+
+        long max = Long.MAX_VALUE;
+        long min = Long.MIN_VALUE;
+
+        System.out.println(Long.toBinaryString(max));
+        System.out.println(Long.toBinaryString(min));
+
+        max = 1 << 31;
+        System.out.println(Long.toString(max));
+        System.out.println(Long.toBinaryString(max));
+
+    }
 
     @Test
     public void and() {
@@ -41,4 +102,9 @@ public class BitOperTest {
 
     }
 
+    @Test
+    public void xor() {
+
+
+    }
 }
