0d4b03054da5594ae877947c6cf7ea5410adda94
xiao <abuzhi@163.com>
2019-07-16 22:45:02 +0800

    bit

---
+++ coding-demo/src/main/java/com/xiao/demo/T131_Palindrome_Partitioning.java
@@ -0,0 +1,32 @@
+package com.xiao.demo;
+
+import java.util.List;
+
+/**
+ * 分割回文串
+ * 给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
+ *
+ * 返回 s 所有可能的分割方案。
+ *
+ * 示例:
+ *
+ * 输入: "aab"
+ * 输出:
+ * [
+ *   ["aa","b"],
+ *   ["a","a","b"]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-partitioning
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T131_Palindrome_Partitioning {
+
+    public List<List<String>> partition(String s) {
+
+        // TODO: 2019/7/16
+
+        return null;
+    }
+}
