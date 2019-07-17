618e18d8b8b5193dfafdd145b9354ca4b42d6ae1
tianxiaoliang <tianxiaoliang@meituan.com>
2019-07-17 21:16:09 +0800

    test

--- coding-demo/src/main/java/com/xiao/demo/T4_Median_of_Two_Sorted_Arrays.java
+++ coding-demo/src/main/java/com/xiao/demo/T4_Median_of_Two_Sorted_Arrays.java
@@ -4,21 +4,28 @@ import java.text.DecimalFormat;
 
 /**
  * Created by xiaoliang on 2017/3/26.
- * <p>
- * There are two sorted arrays nums1 and nums2 of size m and n respectively.
- * <p>
- * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
- * <p>
- * Example 1:
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ *
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ *
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ *
+ * 示例 1:
+ *
  * nums1 = [1, 3]
  * nums2 = [2]
- * <p>
- * The median is 2.0
- * Example 2:
+ *
+ * 则中位数是 2.0
+ * 示例 2:
+ *
  * nums1 = [1, 2]
  * nums2 = [3, 4]
- * <p>
- * The median is (2 + 3)/2 = 2.5
+ *
+ * 则中位数是 (2 + 3)/2 = 2.5
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * 重点是归并排序，适合排序已经排好的两个数组之间
  */
