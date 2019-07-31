2df37970217ce821069b1aa079e84ecec03253ac
xiao <abuzhi@163.com>
2019-07-31 23:33:13 +0800

    122

---
+++ coding-demo/src/main/java/com/xiao/demo/T50_Pow_x_n.java
@@ -0,0 +1,64 @@
+package com.xiao.demo;
+
+/**
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+ *
+ * 示例 1:
+ *
+ * 输入: 2.00000, 10
+ * 输出: 1024.00000
+ * 示例 2:
+ *
+ * 输入: 2.10000, 3
+ * 输出: 9.26100
+ * 示例 3:
+ *
+ * 输入: 2.00000, -2
+ * 输出: 0.25000
+ * 解释: 2-2 = 1/22 = 1/4 = 0.25
+ * 说明:
+ *
+ * -100.0 < x < 100.0
+ * n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/powx-n
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T50_Pow_x_n {
+    public double myPow(double x, int n) {
+
+        return 0;
+    }
+
+    /**
+     * 分治法
+     * 递归
+     * @param x
+     * @param n
+     * @return
+     */
+    public double test1(double x, int n) {
+
+        return 0;
+    }
+
+    /**
+     * 分治法
+     * 迭代
+     * @param x
+     * @param n
+     * @return
+     */
+    public double test2(double x, int n) {
+
+        return 0;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // TODO: 2019/7/30  
+    }
+}
