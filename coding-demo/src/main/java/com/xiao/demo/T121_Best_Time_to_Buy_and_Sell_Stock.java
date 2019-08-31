93dc0517908f8b85fa4aab3ebacdeccbbe968e98
xiao <abuzhi@163.com>
2019-08-31 20:34:01 +0800

    121

---
+++ coding-demo/src/main/java/com/xiao/demo/T121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,68 @@
+package com.xiao.demo;
+
+/**
+ * 买卖股票的最佳时机
+ *
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ *
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ *
+ * 注意你不能在买入股票前卖出股票。
+ *
+ * 示例 1:
+ *
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * 示例 2:
+ *
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class T121_Best_Time_to_Buy_and_Sell_Stock {
+    /**
+     * 如果用两层遍历的方法来暴力获取的话，时间复杂度会是n^2
+     * 此题发现问题点在于：
+     * 每天只能一笔交易
+     * 所以最大利润应该是最近几天的波峰和波谷的差
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        if(prices==null || prices.length <2){
+            return 0;
+        }
+
+        int min = prices[0];
+        int result = 0;
+
+        for(int k : prices){
+            if(k < min){
+                min = k;
+            }else if( k - min > result ){
+                result = k - min;
+            }
+        }
+
+        return result ;
+    }
+
+    public static void main(String[] args) {
+
+        int[] data = {7,1,5,3,6,4};
+        int[] data2 = {7,6,4,3,1};
+        T121_Best_Time_to_Buy_and_Sell_Stock test = new T121_Best_Time_to_Buy_and_Sell_Stock();
+        System.out.println(test.maxProfit(data));
+        System.out.println(test.maxProfit(data2));
+
+
+    }
+}
